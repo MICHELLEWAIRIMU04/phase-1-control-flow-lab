827af90ddd08b64511d704a209aa92e1efe6e50e
MICHELLEWAIRIMU04 <michelle@michelle>
2024-09-27 11:00:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+/*function scuberGreetingForFeet(ride) {
+  if(ride <= 400) {
+    return 'This is on me!'
+  }
+  else if(ride > 400 && ride < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if(ride > 2000 && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+console.log(scuberGreetingForFeet(2501))
+*/ 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+//function ternaryCheckCity() {
+const city = 'NYC'
+console.log(city === 'Pittsburgh' ? 'Ok, sounds good.' : 'No go.')
+
+
+/*function switchOnCharmFromTip(tip){
+  switch (tip){
+  case 'generous':
+    return 'Thank you so much.'
+  break ;
+
+  case 'not generous':
+    return 'Thank you.'
+  break ;
+
+  default:
+    return 'Bye.'
+  
 }
+}
+console.log(switchOnCharmFromTip('Thanks for everythng'))
+*/
